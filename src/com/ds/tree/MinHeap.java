8752d167a5580d0c2038811b8778784e21bd5989
arvind8692 <arvind8692@shivay>
2016-10-26 00:18:38 +0530

    commit

--- src/com/ds/tree/MinHeap.java
+++ src/com/ds/tree/MinHeap.java
@@ -18,10 +18,7 @@ public class MinHeap {
 		int i=size-1;
 		harr[i]=value;
 		
-		while(i!=0 && harr[parent(i)]>harr[i]){
-			swap(harr,i,parent(i));
-			i=parent(i);
-		}
+		fixUpwards(size-1);
 	return true;
 	}
 	private void swap(int[] harr2, int i, int parent) {
@@ -39,7 +36,112 @@ public class MinHeap {
 	private int right(int i){
 		return 2*i+2;	
 	}
-	public static void main (String args[]){
+	private int getMin() {
+		if(size <= 0) {
+			System.out.println("Heap underflow");
+			return Integer.MAX_VALUE;
+		}
+		return harr[0];
+	}
+	private int extractMin(){
+		if(size<=0){
+			System.out.println("Heap underflow");
+			return Integer.MAX_VALUE;
+		}
+		if(size==1) {
+			size--;
+			return harr[0];
+		}
+		int root=harr[0];
+		harr[0]=harr[size-1];
+		minHeapify(0);
 		
+		return root;
+	}
+	private void minHeapify(int i) {
+		int l = left(i);
+		int r = right(i);
+		int smallest = i;
+		if(l<size && harr[l] <harr[smallest]){
+			smallest=l;
+		}
+		if(r<size && harr[l]<harr[smallest]){
+			smallest=r;
+		}
+		if(smallest!=i){
+			swap(harr,i,smallest);
+			minHeapify(smallest);
+		}
+	}
+	private void delete(int i) {
+		decreaseKey(i,Integer.MIN_VALUE);
+		extractMin();
+	}
+	private void decreaseKey(int i, int newKey) {
+		harr[i] = newKey;
+		fixUpwards(i); 
+	}
+	
+	private void fixUpwards(int i) {
+		while(i != 0 && harr[i] < harr[parent(i)]) {
+			swap(harr,i,parent(i));
+			i = parent(i);
+		}
+	}
+	private void printMinHeap() {
+		for(int i=0;i<size;i++)
+			System.out.print(harr[i] + " ");
+		System.out.println();
+	}
+	public static void main (String args[]){
+		MinHeap mh = new MinHeap(5);
+		 
+		mh.insert(3);
+		mh.printMinHeap();
+		System.out.println();
+ 
+		mh.insert(2);
+		mh.printMinHeap();
+		System.out.println();
+ 
+		mh.delete(1);
+		mh.printMinHeap();
+		System.out.println();
+ 
+		mh.insert(15);
+		mh.printMinHeap();
+		System.out.println();
+ 
+		mh.insert(5);
+		mh.printMinHeap();
+		System.out.println();
+ 
+		mh.insert(4);
+		mh.printMinHeap();
+		System.out.println();
+ 
+		mh.insert(45);
+		mh.printMinHeap();
+		System.out.println();
+ 
+		mh.insert(50);
+		mh.printMinHeap();
+		System.out.println();
+ 
+		System.out.println("Extracted min: " + mh.extractMin());
+		mh.printMinHeap();
+		System.out.println();
+ 
+		System.out.println("Min: " + mh.getMin());
+		mh.printMinHeap();
+		System.out.println();
+ 
+		mh.decreaseKey(2,1);
+		mh.printMinHeap();
+		System.out.println();
+ 
+		System.out.println("Min: " + mh.getMin());
+		mh.printMinHeap();
+
 	}
 }
